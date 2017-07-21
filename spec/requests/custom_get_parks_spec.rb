cbc46dfcbabdf697a402e0050062bd0081b71e05
Grady L Shelton <gradyish@gmail.com>
2017-07-21 13:50:28 -0700

    pass tests for search by ownership types

--- spec/requests/custom_get_parks_spec.rb
+++ spec/requests/custom_get_parks_spec.rb
@@ -47,3 +47,16 @@ describe "get parks by acreage search route", :type => :request do
     expect(response).to have_http_status(:success)
   end
 end
+
+describe "get parks by ownership search route", :type => :request do
+  let!(:parks) { FactoryGirl.create_list(:park, 20)}
+  before { get '/parks?ownership=National'}
+
+  it 'returns every park with matching ownership type' do
+    expect(JSON.parse(response.body).size).to eq(20)
+  end
+
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
