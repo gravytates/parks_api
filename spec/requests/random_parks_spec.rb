4775fb387736d355300afdb9c94b200a9c6483b6
Grady L Shelton <gradyish@gmail.com>
2017-07-21 13:44:06 -0700

    test random and name get requests

--- spec/requests/random_parks_spec.rb
+++ spec/requests/random_parks_spec.rb
@@ -6,8 +6,19 @@ describe "get random parks route", :type => :request do
 
   before { get '/parks?random=true'}
 
+  it 'returns status code 200' do
+    expect(response).to have_http_status(:success)
+  end
+end
+
+describe "get parks by name search route", :type => :request do
+  let!(:parks) { FactoryGirl.create_list(:park, 20)}
+
+
+  before { get '/parks?name=death'}
+
   it 'returns random park' do
-    expect(JSON.parse(response.body).size).to eq(1)
+    expect(JSON.parse(response.body).size).to eq(20)
   end
 
   it 'returns status code 200' do
