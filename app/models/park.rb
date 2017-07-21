c194240b33d2fe333a23e88e97ae1215a8103c65
Grady L Shelton <gradyish@gmail.com>
2017-07-21 12:04:10 -0700

    add scope methods to model to search for different park parameters

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,2 +1,12 @@
 class Park < ApplicationRecord
+
+  validates :name, :state, :acres, :ownership, presence: true
+
+  scope :search_by_name, -> (name) { where("lower(name) like ?", "%#{name.downcase}%") }
+
+  scope :search_by_state, -> (state) { where("lower(state) like ?", "%#{state.downcase}%") }
+
+  scope :search_by_acres, -> (acres) { where("acres = ?", acres ) }
+
+  scope :search_by_ownership, -> (ownership) { where("lower(ownership) like ?", "%#{ownership.downcase}%") }
 end
--- spec/requests/patch_parks_spec.rb
+++ spec/requests/patch_parks_spec.rb
@@ -5,13 +5,9 @@ describe 'update a park', type: :request do
   before do
     park = FactoryGirl.create(:park)
     put "/parks/#{park.id}", params: { name: "new name" }
-    #
-    # breed = FactoryGirl.create(:breed)
-    # put "/v1/breeds/#{breed.id}/?name=tom cat&app_id=#{@user_api_key}"
   end
 
   it 'updates name of park from the api database' do
-    binding.pry
     expect(JSON.parse(response.body)['message']).to eq "Your park has successfully been updated."
   end
 end
